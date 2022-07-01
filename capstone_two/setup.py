ec3ddafdd3dde2d1e3cd8fff2344dce786c643a1
Christopher Cavnor <ccavnor@gmail.com>
2022-07-01 15:53:42 -0700

    capstone two
    capstone two project files and data wrangling jupyter notebook

---
+++ capstone_two/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Springboard capstone two project',
+    author='Christopher Cavnor',
+    license='MIT',
+)
